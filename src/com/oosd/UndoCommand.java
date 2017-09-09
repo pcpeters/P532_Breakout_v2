be0622f8da3d4cf3fc0bfb14373ea732d7d23902
sp60 <Seohyun1@shawns-macbook>
2017-09-09 15:27:42 -0400

    Needs a undoOneStep function in the GameObservable.

--- src/com/oosd/UndoCommand.java
+++ src/com/oosd/UndoCommand.java
@@ -1,5 +1,22 @@
 package com.oosd;
 
-public class UndoCommand {
-
+public class UndoCommand implements Command {
+	private Object stateReceiver;
+	
+	public UndoCommand(Object stateReceiver) {
+		if (stateReceiver instanceof GameObservable)
+			this.stateReceiver = stateReceiver;
+	}
+	
+	public void setStateReceiver(Object stateReceiver) {
+		this.stateReceiver = stateReceiver;
+	}
+	
+	public Object getStateReceiver() {
+		return stateReceiver;
+	}
+	
+	public void execute() {
+		((GameObservable) stateReceiver).undoOneStep();
+	}
 }
